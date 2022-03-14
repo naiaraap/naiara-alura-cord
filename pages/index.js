70d6261af4baba97b6cd02163c4e32b06fb6e68a
naiaraap <naiarapantuza@gmail.com>
2022-03-14 09:20:43 -0300

    140322

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
+
 function Titulo(props) {
   const Tag = props.tag || 'h1';
   return (
@@ -19,6 +20,7 @@ function Titulo(props) {
   );
 }
 
+
 // Componente React
 // function HomePage() {
 //     // JSX
@@ -33,9 +35,44 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  // const username = 'omariosouto';
-  const [username, setUsername] = React.useState('omariosouto');
+  const [username, setUsername] = React.useState('naiaraap');
   const roteamento = useRouter();
+  const usernamelenght = username.length;
+  console.log(usernamelenght);
+  function getUserStatus ( usernamelenght ) {
+
+      // Determine if the user image must be shown
+      if (usernamelenght < 2 ){
+        return "Invalid user";
+      } else {
+        return "Show user information!";
+              }
+      }
+
+      console.log(getUserStatus (usernamelenght));
+
+  // async function getMoviesFromApi() {
+  //   try {
+  //     let response = await fetch(``);
+  //     let responseJson = await response.json();
+  //     return responseJson.movies;
+  //     } catch(error) {
+  //     console.error(error);
+  //   }
+  // }
+  
+
+  let imageURL = `https://github.com/${username}.png`;
+  // get proper image
+  if (getUserStatus (usernamelenght) === "Invalid user"){
+          imageURL = ``
+        };
+
+  let exibedName = username;
+  if (usernamelenght < 2 ) {
+    exibedName = ``
+  };
+
 
   return (
     <>
@@ -67,10 +104,13 @@ export default function PaginaInicial() {
             as="form"
             onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
-              console.log('Alguém submeteu o form');
-              roteamento.push('/chat');
-              // window.location.href = '/chat';
+              console.log("Alguém submeteu o form");
+              // window.location.href = '/chat'
+              if (usernamelenght >= 2) {
+                roteamento.push('/chat');
+              }
             }}
+
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -81,18 +121,21 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
-            {/* <input
-                            type="text"
-                            value={username}
-                            onChange={function (event) {
-                                console.log('usuario digitou', event.target.value);
-                                // Onde ta o valor?
-                                const valor = event.target.value;
-                                // Trocar o valor da variavel
-                                // através do React e avise quem precisa
-                                setUsername(valor);
-                            }}
-                        /> */}
+            
+            {/* <input type="text" 
+            value = {username}
+            onChange = {function handler(event) {
+              console.log('usuário digitou', event.target.value)
+              // Onde está o valor?
+              const valor = event.target.value;
+              // Trocar o valor da variável
+              // Através do react
+              setUsername(valor);
+            }} */}
+            {/* /> */}
+
+
+          
             <TextField
               value={username}
               onChange={function (event) {
@@ -100,7 +143,7 @@ export default function PaginaInicial() {
                 // Onde ta o valor?
                 const valor = event.target.value;
                 // Trocar o valor da variavel
-                // através do React e avise quem precisa
+                // através do React e avisa que precisa
                 setUsername(valor);
               }}
               fullWidth
@@ -149,8 +192,12 @@ export default function PaginaInicial() {
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${username}.png`}
+
+              src={imageURL}
             />
+            <div>
+              {getUserStatus}
+            </div>
             <Text
               variant="body4"
               styleSheet={{
@@ -160,7 +207,7 @@ export default function PaginaInicial() {
                 borderRadius: '1000px'
               }}
             >
-              {username}
+              {exibedName}
             </Text>
           </Box>
           {/* Photo Area */}
